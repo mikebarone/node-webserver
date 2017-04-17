09122fff57c09f20a3a2bdc4dc33ccbfd27a7cde
mikebarone <mikebarone@gmail.com>
2017-04-16 22:35:17 -0400

    Projects page

--- server.js
+++ server.js
@@ -58,6 +58,13 @@ app.get('/bad',(req, res) => {
     });
 });
 
+app.get('/projects',(req, res)=>{
+    res.render('projects.hbs', {
+        pageTitle: 'Projects',
+        projectsMessage: 'My portfolio... soon it will updated'
+    });
+});
+
 // Listen port for webserver
 app.listen(port,() => {
     console.log(`Server is up on port ${port}`);
